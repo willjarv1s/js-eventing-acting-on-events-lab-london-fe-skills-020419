299233174d1a737207e40f3ba1e38b31503cdc79
Will Jarvis <will@toohotlimited.com>
2019-03-20 19:22:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ dodger.style.width = "100px"
 
 dodger.style.bottom = '0px'
 
-
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
 
 
