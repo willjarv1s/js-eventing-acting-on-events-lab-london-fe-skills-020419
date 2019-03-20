be843f70971c7d8e3447541384b3fb9cd482edde
Will Jarvis <will@toohotlimited.com>
2019-03-20 19:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ document.addEventListener('keydown', function(e) {
     moveDodgerLeft()
   }
 })
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
