8b4afc46769c63394755a4ab406ad32e71172fab
Will Jarvis <will@toohotlimited.com>
2019-03-20 19:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
  
-  if (left > 360) {
+  if (left >= 360) {
     dodger.style.left = `${left + 1}px`
   }
   else if (left > 0) {
